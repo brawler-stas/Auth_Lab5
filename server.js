ee40ffc8f52af3c4c8768c5dd9c85aa4b87e1b25
brawler-stas <120058696+brawler-stas@users.noreply.github.com>
2025-12-29 19:10:18 +0300

    alsndljas

--- server.js
+++ server.js
@@ -229,7 +229,7 @@ app.get('/api/profile', authenticateWithIp, async (req, res) => {
 app.get('/api/public', optionalAuth, (req, res) => {
   res.json({
     success: true,
-    message: 'Публичный маршрут2222222222222222',
+    message: 'Публичный маршрут3333333333333',
     authenticated: !!req.user,
     user: req.user ? {
       username: req.user.username,
